d99de87d15898becf8c1134072431213851dd6d8
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2025-02-11 16:48:05 -0600

    Update dssp_command.R

--- R/dssp_command.R
+++ R/dssp_command.R
@@ -20,6 +20,9 @@ dssp_command <- function(pdb_file) {
   output_file <- gsub("\\.pdb.gz|\\.pdb", ".dssp", pdb_file)
 
   # Run the DSSP command with mkdssp, return error message if an error occurs.
+
+  print(.findExecutable("mkdssp"))
+  
   #system2(
   #    command = .findExecutable(mkdssp),
   #    args = c("-i", pdb_file, "-o", output_file),
