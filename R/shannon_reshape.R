cc8f12dde3a95762d61ca1067e3768f72ffa17e1
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2025-06-16 13:39:05 -0500

    Update shannon_reshape.R

--- R/shannon_reshape.R
+++ R/shannon_reshape.R
@@ -21,7 +21,7 @@ shannon_reshape <- function(msa, query) {
   ind <- which(chars != "-")
 
   # Calculate Shannon entropy for each column of the MSA
-  score <- apply(mat, 2, shannon_entropy, nogap = TRUE)
+  score <- apply(mat, 2, shannon_entropy, nogap = FALSE)
 
   # Return Shannon entropy for only query positions
   temp <- score[ind]
